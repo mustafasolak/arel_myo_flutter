979e62380df93df83287587720ff95ecde148255
Mustafa SOLAK <mustafasolak22@hotmail.com>
2022-03-24 12:27:35 +0300

    1_Değişkenler
    Değişken türleri anlatıldı.

---
+++ 1_degiskenler.dart
@@ -0,0 +1,50 @@
+void main(){
+  
+  print("Ali");
+  print("Selam ben geldim :)");
+  
+  String isim;
+  isim = "Hakan";
+  
+  print(isim);
+  
+  String isiM = "Ali";
+  
+  String? ogrenciNo;
+  print(ogrenciNo);
+  
+  // dynamic -> hem türü hem değeri değişebilir
+  dynamic veri = "Ali";
+  veri = "Ahmet";
+  veri = 45.3;
+ 
+  // var -> içine ilk atılan tür ne ise hep onu ister
+  var msVeri = 45;
+  msVeri = 34;
+  // msVeri = "Ahmet"; // Hata verir
+  
+  final API_KEY = "ada243243dsfs35=2424";
+  // API_KEY = "ali";
+  // API_KEY = 14;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
