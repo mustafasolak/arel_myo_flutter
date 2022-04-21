6c47ed213b2a05e01df553549a972f64f2ae70ed
Mustafa SOLAK <mustafasolak22@hotmail.com>
2022-04-21 09:50:28 +0300

    Add files via upload

--- proje1/ilkproje/lib/14_uygulama_1.dart
+++ proje1/ilkproje/lib/14_uygulama_1.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
+enum SingingCharacter { lafayette, jefferson }
+
+enum Modeller { volkswagen, renault, honda, hyundai }
+
 void main() {
+  /*var gelenVeri = "şahin";
+  if(gelenVeri == Modeller.honda){
+
+  }
+
+  if(gelenVeri == "honda"){
+
+  }*/
+
   runApp(
     MaterialApp(
       home: Scaffold(
@@ -16,9 +29,15 @@ void main() {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Delivery Address"),
+                  Text(
+                    "Delivery Address",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
                   SizedBox(
-                    height: 15,
+                    height: 20,
                   ),
                   Row(
                     children: [
@@ -37,17 +56,109 @@ void main() {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("20845 Oakfridge Farm Lane"),
+                          Text(
+                            "20845 Oakfridge Farm Lane",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
                           SizedBox(
                             height: 15,
                           ),
-                          Text("New York (NYC)"),
+                          Text(
+                            "New York (NYC)",
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ],
                       ),
                       Spacer(),
                       Icon(Icons.keyboard_arrow_right),
                     ],
-                  )
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Payment Method",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  ListTile(
+                    leading: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(
+                        Icons.apple,
+                        color: Colors.black,
+                      ),
+                    ),
+                    title: Text("Apple Pay"),
+                    subtitle: Text(
+                      "©©©© ©©©© 0357 8420",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(
+                        Icons.credit_card,
+                        color: Colors.red,
+                      ),
+                    ),
+                    title: Text("Master Card"),
+                    subtitle: Text(
+                      "©©©© ©©©© 0357 8420",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(
+                        Icons.wallet_giftcard,
+                        color: Colors.green,
+                      ),
+                    ),
+                    title: Text("Apple Pay"),
+                    subtitle: Text(
+                      "©©©© ©©©© 0357 8420",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("My Cart"),
+                      Icon(Icons.keyboard_arrow_right),
+                    ],
+                  ),
                 ],
               ),
             ),
